ba66b7c4c4c2780baa2e76425b2914cde9d5b29c
Gabriel Buhrer <gabrielpb1@al.insper.edu.br>
2024-04-23 11:40:13 -0300

    app bar

---
+++ notes-frontend/src/components/AppBar/index.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+
+export default function AppBar() {
+    return (
+        <div className="appbar">
+            <img src="logo.png" className="logo" />
+            <span className="subtitle">Meu Game Favorito</span>
+        </div>
+    );
+}
