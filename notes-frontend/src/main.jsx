a6dc99de76de946624e96ae37ff6bc84d75a32c4
DanielMarcoD <danielmd3@al.insper.edu.br>
2024-04-30 10:11:49 -0300

    Att

--- notes-frontend/src/main.jsx
+++ notes-frontend/src/main.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from './App.jsx'
-import JogosFav from './components/JogosFav/index.jsx'
-import Buscador from './components/Buscador/index.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import JogosFav from './components/JogosFav/index.jsx';
+import Buscador from './components/Buscador/index.jsx';
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
   {
-    path: "/favoritos",
+    path: '/favoritos',
     element: <JogosFav />,
   },
   {
-    path: "/buscador",
+    path: '/buscador',
     element: <Buscador />,
-  }
+  },
 ]);
 
+// Somente use ReactDOM.createRoot() sem ReactDOM.render() para criar a raiz de renderização
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
