7db248e9e801b2a1dd231e1021d1d5968426cb4b
DanielMarcoD <danielmd3@al.insper.edu.br>
2024-05-10 13:12:06 -0300

    Update Edit User

--- src/components/EditaUsuario/index.jsx
+++ src/components/EditaUsuario/index.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './index.css';
 import axios from 'axios';
+import AppBarFavBusca from "../AppBarFAvBusca/index.jsx";
+import { useNavigate } from 'react-router-dom';
 
 const EditUserInfo = () => {
+  const[isSaved, setIsSaved] = useState(false);
+  const[isDeleted, setIsDeleted] = useState(false);  
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
   const config = {
     headers: {
       Authorization: `token ${localStorage.getItem("token")}`,
@@ -28,6 +33,7 @@ const EditUserInfo = () => {
   
 
   const handleSave = () => {
+    setIsSaved(true);
    
 
     axios.post('http://127.0.0.1:8000/api/infos/', {"username":name,"password":password,"email":email},config)
@@ -37,9 +43,33 @@ const EditUserInfo = () => {
       .catch(error => {
         console.error('Erro ao atualizar usuário:', error);
       });
+  
   };
 
+  const handleDelete = () => {
+
+    setIsDeleted(true);
+    axios.delete('http://127.0.0.1:8000/api/infos/', config)
+      .then(response => {
+        console.log('Atualização realizada com sucesso:', response.data);
+      })
+      .catch(error => {
+        console.error('Erro ao atualizar usuário:', error);
+      });
+  
+  };
+
+  if (isSaved) {
+    return navigate('/principal');
+  }
+  if (isDeleted) {
+    return navigate('/');
+  }
+
   return (
+  
+    <>
+    <AppBarFavBusca/>
     <div className="edit-user-info">
       <h2>Edit User Information</h2>
       <div>
@@ -66,8 +96,11 @@ const EditUserInfo = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <button onClick={handleSave}>Save</button>
+      <button className='button_save_user' onClick={handleSave}>Save</button>
+      <button className='button_delete_user' onClick={handleDelete}>Delete</button>
     </div>
+    </>
+    
   );
 };
 
